72d26747d7937d500be2fd5d02ac71eab26fd36b
Eric Pascual <eric@pobot.org>
2014-11-16 19:42:52 +0100

    input demo added

--- pybot/abelectronics/test/test_iopi.py
+++ pybot/abelectronics/test/test_iopi.py
@@ -7,22 +7,37 @@ from pybot.abelectronics.iopi import IOPiBoard
 from pybot import raspi
 
 import time
+import sys
 
 print("""
 This example blinks a LED connected between pin 8 of IC_1 header and the ground.
+It also reads the state of a button connected between pin 1 of IC_1 header and the ground.
+
 Hit Ctrl-C to terminate.
 """)
 
 board = IOPiBoard(raspi.i2c_bus)
-led = board.get_digital_output(8)
+led = board.get_digital_output(IOPiBoard.EXPANDER_1, 8)
+button = board.get_digital_input(IOPiBoard.EXPANDER_1, 1, pullup_enabled=True)
+
+
+def display_button_state():
+    print("\033[30Dbutton state = %s" % button.is_set()),
+    sys.stdout.flush()
 
 try:
+    on = True
     while True:
-        led.set()
-        time.sleep(0.5)
-        led.clear()
+        if on:
+            led.set()
+        else:
+            led.clear()
+
+        display_button_state()
+        on = not on
         time.sleep(0.5)
 
 except KeyboardInterrupt:
     print("\nCtrl-C caught. Terminating program")
+    led.clear()
 
